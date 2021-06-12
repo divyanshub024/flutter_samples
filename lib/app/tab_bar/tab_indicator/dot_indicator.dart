4be81ec1d5b7ce0840b866460e01462df3a5215c
Divyanshu Bhargava <divyanshub024@gmail.com>
2021-06-12 16:32:15 +0530

    Add dot indicator class.

---
+++ lib/app/tab_bar/tab_indicator/dot_indicator.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class DotIndicator extends Decoration {
+  const DotIndicator({
+    this.color = Colors.white,
+    this.radius = 4.0,
+  });
+
+  final Color color;
+  final double radius;
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _DotPainter(
+      color: color,
+      radius: radius,
+      onChange: onChanged,
+    );
+  }
+}
+
+class _DotPainter extends BoxPainter {
+  _DotPainter({
+    required this.color,
+    required this.radius,
+    VoidCallback? onChange,
+  })  : _paint = Paint()
+          ..color = color
+          ..style = PaintingStyle.fill,
+        super(onChange);
+
+  final Paint _paint;
+  final Color color;
+  final double radius;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    assert(configuration.size != null);
+    final Rect rect = offset & configuration.size!;
+    canvas.drawCircle(
+        Offset(rect.bottomCenter.dx, rect.bottomCenter.dy - radius),
+        radius,
+        _paint);
+  }
+}
