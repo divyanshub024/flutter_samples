203fdf01871b68cf4b77d1f3ffacf9b826fc4b39
Divyanshu Bhargava <divyanshub024@gmail.com>
2021-06-12 16:32:33 +0530

    Add outline indicator class.

---
+++ lib/app/tab_bar/tab_indicator/outline_indicator.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class OutlineIndicator extends Decoration {
+  const OutlineIndicator({
+    this.color = Colors.white,
+    this.strokeWidth = 2,
+    this.radius = Radius.zero,
+  });
+
+  final Color color;
+  final double strokeWidth;
+  final Radius radius;
+
+  @override
+  BoxPainter createBoxPainter([VoidCallback? onChanged]) {
+    return _OutlinePainter(
+      color: color,
+      strokeWidth: strokeWidth,
+      radius: radius,
+      onChange: onChanged,
+    );
+  }
+}
+
+class _OutlinePainter extends BoxPainter {
+  _OutlinePainter({
+    required this.color,
+    required this.strokeWidth,
+    required this.radius,
+    VoidCallback? onChange,
+  })  : _paint = Paint()
+          ..style = PaintingStyle.stroke
+          ..color = color
+          ..strokeWidth = strokeWidth,
+        super(onChange);
+
+  final Color color;
+  final double strokeWidth;
+  final Radius radius;
+  final Paint _paint;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration configuration) {
+    assert(configuration.size != null);
+    var rect = offset & configuration.size!;
+    var rrect = RRect.fromRectAndRadius(rect, radius);
+    canvas.drawRRect(rrect, _paint);
+  }
+}
