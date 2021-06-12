a4ae24b82d1d12de6cf2019705b749f81edb99cd
Divyanshu Bhargava <divyanshub024@gmail.com>
2021-06-12 16:29:42 +0530

    Add themes and colors.

---
+++ lib/values/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = const Color(0xFF181818);
+  static const accentColor = const Color(0xFF594EF3);
+  static const bgColor = const Color(0xFF181818);
+  static const elevation1Color = const Color(0xFF212121);
+  static const elevation2Color = const Color(0xFF2A2A2A);
+  static const elevation3Color = const Color(0xFF343434);
+}
